b1a530febbddd5a9c50d6410cb3dec63e0a20123
Ahmad Nasaiei <ahmad.nasaiei@gmail.com>
2025-05-08 22:04:51 +0800

    Feat: Product card in Product Gallery

--- pallet-pine-terengganu-web/src/pages/ProductGalleryPage/ProductGalleryPage.tsx
+++ pallet-pine-terengganu-web/src/pages/ProductGalleryPage/ProductGalleryPage.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import s from "./ProductGalleryPage.module.css";
 import { useProductStore } from "../../store/useProductStore";
+import placeholderImg from "../../assets/img/product-1.jpg";
 
 interface ProductStore {
   products: any[];
@@ -30,7 +31,7 @@ function ProductGalleryPage() {
     <div className={s.mainContainer}>
       <div className={s.leftColumn}>
         <div className={s.filterContainer}>
-          <h1>Kategori</h1>
+          <h3>Kategori</h3>
           <div className={s.filterCheckboxContainer}>
             {category.map((item, idx) => (
               <div key={idx} className={s.filterCheckbox}>
@@ -47,19 +48,24 @@ function ProductGalleryPage() {
       </div>
 
       <div className={s.rightColumn}>
-        {products.map((item, idx) => (
-          <div key={idx} className={s.productContainer}>
-            <div className={s.productCard}>
+        <div className={s.productHeader}>
+          <h1>Produk Kami</h1>
+          <span>{products.length} Produk</span>
+        </div>
+        <div className={s.productContainer}>
+          {products.map((item, idx) => (
+            <div key={idx} className={s.productCard}>
               <div className={s.productImg}>
-                {/* <img key={idx} src={item.img} alt="" /> */}
+                <img src={item.imgPath || placeholderImg} alt={item.item} />
               </div>
               <div className={s.productDetail}>
-                <h1>{item.item}</h1>
+                <span>{item.category}</span>
+                <h3>{item.item}</h3>
                 <p>{item.size}</p>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
